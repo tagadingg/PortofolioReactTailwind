f4bf75ce5a81221995c9fb5cf35b397ed5a5f8a2
tanziilaag <tanziilaag@gmail.com>
2025-10-02 03:05:36 +0700

    nambahin footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => {
         <a href="#beranda">Beranda</a>
         <a href="#tentang">Tentang</a>
         <a href="#proyek">Proyek</a>
+        <a href="#sertifikat">Sertifikat</a>
       </div>
       <div className="flex items-center gap-3">
         <a
